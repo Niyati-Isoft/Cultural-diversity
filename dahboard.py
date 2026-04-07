b3d00bcdc049b24a486a5586062f20f9909ae0cd
Niyati-Isoft <niyati@isoftanz.com.au>
2026-04-07 11:33:50 +1000

    Update dahboard.py

--- dahboard.py
+++ dahboard.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import streamlit as st
+
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == "mysecure123":
+            st.session_state["password_correct"] = True
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("🔐 Enter Password", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("🔐 Enter Password", type="password", on_change=password_entered, key="password")
+        st.error("❌ Incorrect password")
+        return False
+    else:
+        return True
+
+# STOP APP IF NOT AUTHENTICATED
+if not check_password():
+    st.stop()
 ##
 # -------------------------------
 # PAGE CONFIG
